7945870e7529b4be3ecc1bee57d5bcd37ab9c617
Victor <1606427136@qq.com>
2022-05-13 16:34:25 +0800

    Add difficulty setting

--- MouseEventThing/Mouse.c
+++ MouseEventThing/Mouse.c
@@ -5,46 +5,84 @@
 
 main()
 {
-	int i = 0, ClickScore = 0, CsrPosX = 44, CsrPosY = 12;
-	HANDLE hIn = GetStdHandle(STD_INPUT_HANDLE);
+	HANDLE hIn = GetStdHandle(STD_INPUT_HANDLE);			//Specify handles
 	HANDLE hOut = GetStdHandle(STD_OUTPUT_HANDLE);
-	INPUT_RECORD InRecord;
-	LPDWORD EvntNum;
+	INPUT_RECORD InRecord;									//Event structure
+#define MousePos InRecord.Event.MouseEvent.dwMousePosition	//Save myself some time
+	LPDWORD EvntNum;										//This is useless but needed anyway
 	CONSOLE_CURSOR_INFO CsrInfo;
-	srand((unsigned)time(NULL));		//Set random seed
-#define MousePos InRecord.Event.MouseEvent.dwMousePosition		//Stupid visual studio
-	SetConsoleMode(hIn, ENABLE_EXTENDED_FLAGS | ENABLE_WINDOW_INPUT | ENABLE_MOUSE_INPUT);	//Enable all the modes
-	CsrInfo.bVisible = 0;	CsrInfo.dwSize = 100;
-	SetConsoleCursorInfo(hOut, &CsrInfo);
 	SetConsoleTitle(TEXT("Click the Cursor!"));
+	short int ClickScore, CsrPosX, CsrPosY, RestartFlag, BoxSizeX, BoxSizeY;
+
+	putchar('\n');
+	puts("1 - Tiny   5 * 5");
+	puts("2 - Easy   17* 9");
+	puts("3 - Medium 33*17");
+	puts("4 - Hard   43*21");
+	puts("Other - Exit");
+	GotoXY(0, 0);
+	printf("Welcome! Please pick a difficulty: [_\b");
+	char Diff = getchar();
+	system("color 0F");
+	switch (Diff)
+	{
+	case '1':
+		system("cls");
+		puts("Sike! Tiny is not working yet! Tiny man!");
+		system("timeout /t 4");
+		return 0;
+	case '2':
+		system("mode con cols=40 lines=15");
+		BoxSizeX = 19, BoxSizeY = 11;
+		break;
+	case '3':
+		system("mode con cols=72 lines=23");
+		BoxSizeX = 35, BoxSizeY = 19;
+		break;
+	case '4':
+		system("mode con cols=92 lines=27");		//Set the correct window size
+		BoxSizeX = 45, BoxSizeY = 23;		//Playfield Restriction:(1,2)~(43,22)	don't forget X*2
+		break;
+	default:
+		return 0;
+	}
+Start:
+	RestartFlag = 0, ClickScore = 0;
+	CsrPosX = BoxSizeX - 1, CsrPosY = BoxSizeY / 2 + 2;			//Middle Position
+	srand((unsigned)time(NULL));								//Set random seed
+	SetConsoleMode(hIn, ENABLE_EXTENDED_FLAGS | ENABLE_WINDOW_INPUT | ENABLE_MOUSE_INPUT);	//Enable all the modes
+	CsrInfo.bVisible = 0;	CsrInfo.dwSize = 100;				//Preset Cursor info
+	SetConsoleCursorInfo(hOut, &CsrInfo);
 	puts("Click the middle cursor to begin!");
-	CreateBox(45, 23);		//Playfield Restriction:(1,2)~(43,22)	don't forget X*2
-	GotoXY(44, 12);			//Middle Position
+	CreateBox(BoxSizeX, BoxSizeY);
+	GotoXY(CsrPosX, CsrPosY);	//Middle Position
 	CsrInfo.bVisible = 1;	SetConsoleCursorInfo(hOut, &CsrInfo);
 
 	while (TRUE)
 	{
-		ReadConsoleInput(hIn, &InRecord, 1, &EvntNum);
-		if (InRecord.Event.MouseEvent.dwButtonState == FROM_LEFT_1ST_BUTTON_PRESSED)
+		ReadConsoleInput(hIn, &InRecord, 1, &EvntNum);			//Scan for input
+		if (InRecord.Event.MouseEvent.dwButtonState == FROM_LEFT_1ST_BUTTON_PRESSED)	//LMB Press
 		{
-			if (InRecord.Event.MouseEvent.dwEventFlags == MOUSE_MOVED)	continue;	//Stop spamming from mousemove event
+			if (InRecord.Event.MouseEvent.dwEventFlags == MOUSE_MOVED)	continue;		//Block mousemove event
+			if (RestartFlag == 1)	break;
 			if (MousePos.X == CsrPosX && MousePos.Y == CsrPosY)
 			{
 				CsrInfo.bVisible = 0;	SetConsoleCursorInfo(hOut, &CsrInfo);
 				GotoXY(0, 0);	printf("                                 \rScore: %d", ++ClickScore);
 				CsrInfo.bVisible = 1;	SetConsoleCursorInfo(hOut, &CsrInfo);
-				GotoXY(CsrPosX = GetRndNumRange(1 * 2, 43 * 2), CsrPosY = GetRndNumRange(2, 22));
+				GotoXY(CsrPosX = GetRndNumRange(1 * 2, (BoxSizeX - 2) * 2), CsrPosY = GetRndNumRange(2, BoxSizeY - 1));
 			}
 			else
 			{
-				GotoXY(0, 0);	printf("                                 \rYou Failed!");
-				CsrInfo.dwSize = 1;	SetConsoleCursorInfo(hOut, &CsrInfo);
-				Sleep(2000);
-				break;
+				GotoXY(0, 0);	printf("                                 \rYou Failed! Score: %d", ClickScore);
+				CsrInfo.dwSize = 1;	CsrInfo.bVisible = 0; SetConsoleCursorInfo(hOut, &CsrInfo);
+				GotoXY(BoxSizeX - 12, BoxSizeY / 2 + 2);				//Middle Pos
+				printf("Click again to restart.");	RestartFlag = 1;	//Next event restart
 			}
 		}
 	}
 	system("cls");
+	goto Start;
 	return 0;
 }
 
